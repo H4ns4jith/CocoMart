5f9afda9603fdb1f6e2361ff072c7d3f5b6422d0
sachinkavindu <139635500+sachinkavindu@users.noreply.github.com>
2024-12-05 19:03:06 +0000

    database connected

---
+++ connection/connection.php
@@ -0,0 +1,8 @@
+<?php
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "mariadb";
+    $database = "cocomart";
+
+    $connection = mysqli_connect($servername,$username,$password,$database);
+?>
